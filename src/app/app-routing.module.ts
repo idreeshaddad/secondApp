b06f60df6a4a32ec1dd721f0c7bc993f5d66c4da
Idrees Haddad <gelnox@live.com>
2023-09-21 21:11:06 +0300

    Added driver component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DriverComponent } from './driver/driver.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "drivers", component: DriverComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
