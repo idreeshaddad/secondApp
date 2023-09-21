b1098876a5b551d57df851d6c4a286544e9cf35e
Idrees Haddad <gelnox@live.com>
2023-09-21 21:25:54 +0300

    - Added Home and Driver components - Added router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DriverComponent } from './driver/driver.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  { path: "", component: HomeComponent },
   { path: "drivers", component: DriverComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { DriverComponent } from './driver/driver.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DriverComponent
+    DriverComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
